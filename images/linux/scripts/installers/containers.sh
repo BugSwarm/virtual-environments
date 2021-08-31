94c43b9f48680aa26b53aceca0e9a267021fb391
Robert-Furth <rmfurth@ucdavis.edu>
2021-08-31 11:43:34 -0700

    modify installer scripts to work with docker containers

--- images/linux/scripts/installers/containers.sh
+++ images/linux/scripts/installers/containers.sh
@@ -9,13 +9,14 @@ source $HELPER_SCRIPTS/os.sh
 install_packages=(podman buildah skopeo)
 REPO_URL="https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable"
 
+set -x
 # Install podman, buildah, scopeo container's tools (on Ubuntu20 these tools can be installed without adding new repository)
 source /etc/os-release
 sh -c "echo 'deb ${REPO_URL}/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
 wget -qnv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
 apt-key add Release.key
 apt-get update -qq
-apt-get -qq -y install ${install_packages[@]}
+apt-get -qq -y -o DPkg::Options::="--force-confold" install ${install_packages[@]}
 mkdir -p /etc/containers
 echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | tee /etc/containers/registries.conf
 
--- images/linux/scripts/installers/mysql.sh
+++ images/linux/scripts/installers/mysql.sh
@@ -20,8 +20,4 @@ apt-get install -y mysql-server
 #Install MySQL Dev tools
 apt install libmysqlclient-dev -y
 
-# Disable mysql.service
-systemctl is-active --quiet mysql.service && systemctl stop mysql.service
-systemctl disable mysql.service
-
 invoke_tests "Databases" "MySQL"
